f0c10fd0718caf3a851a5521fdd6e8573ad3c5a0
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-11-29 17:10:09 +0530

    bottom activel

--- src/components/Navbarbottom.js
+++ src/components/Navbarbottom.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { CgProfile } from 'react-icons/cg';
 import { BiCategory } from 'react-icons/bi';
+import { AiOutlineHome } from 'react-icons/ai';
 
 function Navbarbottom() {
   return (
 <nav className="navbar fixed-bottom navbar-light bg-light">
   <div className="container-fluid">
     <div className='row text-center'>
-    <div className='col '><h5 className='pad'><BiCategory className='active1'/></h5>
+    <div className='col '><h5 className='pad'><AiOutlineHome className='active1'/></h5>
 <p className='sos'>    <a className="navbar-brand a active1" href="#">Store</a></p></div>
-    <div className='col'><h5 className='pad'><BiCategory /></h5>
-    <p className='sos'>   <a className="navbar-brand a" href="#">Categories</a></p></div>
-    <div className='col'><h5 className='pad'><CgProfile /></h5>
-    <p className='sos'>  <a className="navbar-brand a" href="#">Account</a></p></div>
+    <div className='col'><h5 className='pad'><BiCategory className='active2'/></h5>
+    <p className='sos'>   <a className="navbar-brand a active2" href="#">Categories</a></p></div>
+    <div className='col'><h5 className='pad'><CgProfile className='active3'/></h5>
+    <p className='sos'>  <a className="navbar-brand a active3" href="#">Account</a></p></div>
     </div>
   </div>
 </nav>  )
