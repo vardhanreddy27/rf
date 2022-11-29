21dd06bba689b5a22cf75964ac528a78717ca6d5
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-11-29 13:37:49 +0530

    offer

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import tu500 from '../../src/u500.png';
-import top from '../../src/top.png';
+import tshirt from '../../src/tshirt.png';
+import tops from '../../src/tops.png';
 import jeans from '../../src/jeans.png';
 import night from '../../src/night.png';
 import kurta from '../../src/kurta.png';
@@ -14,8 +14,8 @@ function Offers() {
 <h6 className='font fcolor'>Offers Like Never Before </h6>
 
 <div className='row mt-3'>
-<div class="col"><img src={tu500} alt="t shirts" width="100%" height="100%" /></div>
-<div class="col"><img src={top} alt="top" width="100%" height="100%" /></div>
+<div class="col"><img src={tshirt} alt="t shirts" width="100%" height="100%" /></div>
+<div class="col"><img src={tops} alt="top" width="100%" height="100%" /></div>
 <div class="col"><img src={jeans} alt="jeans" width="100%" height="100%" /></div>
 </div>
 <div className='row mt-3'>
