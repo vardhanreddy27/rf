3cd33b0ee361960b2d094cd7931cdb44fba898da
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-11-29 16:56:41 +0530

    bottom active

--- src/components/Navbarbottom.js
+++ src/components/Navbarbottom.js
@@ -7,12 +7,12 @@ function Navbarbottom() {
 <nav className="navbar fixed-bottom navbar-light bg-light">
   <div className="container-fluid">
     <div className='row text-center'>
-    <div className='col '><h5 className='pad'><BiCategory /></h5>
-    <a className="navbar-brand" href="#">Store</a></div>
+    <div className='col '><h5 className='pad'><BiCategory className='active1'/></h5>
+<p className='sos'>    <a className="navbar-brand a active1" href="#">Store</a></p></div>
     <div className='col'><h5 className='pad'><BiCategory /></h5>
-      <a className="navbar-brand" href="#">Categories</a></div>
+    <p className='sos'>   <a className="navbar-brand a" href="#">Categories</a></p></div>
     <div className='col'><h5 className='pad'><CgProfile /></h5>
-      <a className="navbar-brand" href="#">Account</a></div>
+    <p className='sos'>  <a className="navbar-brand a" href="#">Account</a></p></div>
     </div>
   </div>
 </nav>  )
