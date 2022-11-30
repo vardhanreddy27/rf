9f3e15f64c18db1c8a04a41ed0d8c87412c6ab75
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-11-30 10:12:41 +0530

    bottom ass

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,19 @@ import Topnav from '../components/Topnav'
 import banner from '../../src/banner.png';
 import Offers from '../components/Offers';
 import Navbarbottom from '../components/Navbarbottom';
+import Carousel from '../components/Carousel';
 
 function Home() {
   return (
     <>
    <Topnav />
+   <Carousel />
    <Searchbar />
    <div className='fluid-container mt-2'>
    <img src={banner} width="100%" height="100%" /></div>
    <Offers />
    <Navbarbottom />
+
     </>
   )
 }
