962d59c6b768eca76f6ffaa1daca8dd0035424d7
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-12-12 10:37:59 +0530

    ffasnpop

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import ww from '../../src/ww.png'
 import nt from '../../src/nt.png'
+import topt from '../../src/topt.png'
+import trad from '../../src/trad.png'
+import bras from '../../src/bras.png'
 function Menu() {
   return (
     <div className='container ps-4 pe-4 p-3 '>
         <h1>Categories</h1>
        <div className='row'>
-       <div className='col-8 wd'><h4 className='p-5'>Westren Wear</h4></div><div className='col-4 wd'><img className='mt-1' src={ww}alt="f" height={90}/></div>
-       <div className='col-8 wd mt-1'><h4 className='p-5'>Top's & Tee's</h4></div><div className='col-4 wd mt-1'><img className='mt-1' src={ww}alt="f" height={90}/></div>
-       <div className='col-8 wd mt-1'><h4 className='p-4'>Traditional Wear</h4></div><div className='col-4 wd mt-1'><img className='mt-1' src={ww}alt="f" height={90}/></div>
-       <div className='col-8 wd mt-1'><h4 className='p-5'>Night Wear</h4></div><div className='col-4 wd mt-1'><img className='mt-1' src={nt}alt="f" height={90}/></div>
-       <div className='col-8 mt-1 wd'><h4 className='p-5'>Lingerie</h4></div><div className='col-4 wd mt-1'><img className='mt-1' src={ww}alt="f" height={90}/></div>
-       <div className='col-8 mt-1 wd'><h4 className='p-5'>Jeans</h4></div><div className='col-4 wd mt-1'><img className='mt-1' src={ww}alt="f" height={90}/></div>
-       <div className='col-8 mt-1 wd'><h4 className='p-5'>Kurta Sets</h4></div><div className='col-4 wd mt-1'><img className='mt-1' src={ww}alt="f" height={90}/></div>
+       <div className='col-8 wd'><h4 className='ps-4 pop'>Westren Wear</h4></div><div className='col-4 wd float-right'><img className='mt-1 ms-4' src={ww}alt="f" height={90}/></div>
+       <div className='col-8 wd mt-1'><h4 className='ps-4 pop'>Top's & Tee's</h4></div><div className='col-4 wd mt-1'><img className='mt-1 ms-4' src={topt}alt="f" height={90}/></div>
+       <div className='col-8 wd mt-1'><h4 className='ps-4 pop'>Traditional Wear</h4></div><div className='col-4 wd mt-1'><img className='mb-1 ms-4' src={trad}alt="f" height={95}/></div>
+       <div className='col-8 wd mt-1'><h4 className='ps-4 pop'>Night Wear</h4></div><div className='col-4 wd mt-1'><img className='mt-1 ms-4' src={nt}alt="f" height={90}/></div>
+       <div className='col-8 mt-1 wd'><h4 className='ps-4 pop'>Lingerie</h4></div><div className='col-4 wd mt-1'><img className='mt-1 ms-4' src={bras}alt="f" height={90}/></div>
 
         </div> 
 
--- src/components/Navbarbottom.js
+++ src/components/Navbarbottom.js
@@ -9,14 +9,14 @@ function Navbarbottom() {
 <nav className="navbar fixed-bottom navbar-light bg-light">
   <div className="container-fluid">
     <div className='row text-center'>
-    <div className='col '><h5 className='pad'><AiOutlineHome className='active1'/></h5>
+    <div className='col '> <Link to="/" className="navbar-brand a active1" ><h5 className='pad'><AiOutlineHome className='active1'/></h5>
 <p className='sos'>   
-          <Link to="/" className="navbar-brand a active1" >Store</Link></p></div>
-    <div className='col'><h5 className='pad'><BiCategory className='active2'/></h5>
-    <p className='sos'><Link to="/Categories" className="navbar-brand a active2" >Categories</Link></p></div>
-    <div className='col'><h5 className='pad'><CgProfile className='active3'/></h5>
+         Store</p></Link></div>
+    <div className='col'><Link to="/Categories" className="navbar-brand a active2" ><h5 className='pad'><BiCategory className='active2'/></h5>
+    <p className='sos'>Categories</p></Link></div>
+    <div className='col'> <Link to="/Account" className="navbar-brand a active3"  ><h5 className='pad'><CgProfile className='active3'/></h5>
     <p className='sos'> 
-    <Link to="/Account" className="navbar-brand a active3"  >Account</Link></p></div>
+   Account</p></Link></div>
     </div>
   </div>
 </nav>  )
