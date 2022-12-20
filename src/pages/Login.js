7c086075ded5360395936d40f36c58b891dcaf77
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-12-20 17:08:31 +0530

    testmsssddfsaertfdessssslp[ll[sks

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,8 @@
 import React from 'react'
-import { GoogleLoginButton } from "react-social-login-buttons";
-import { LoginSocialGoogle } from "reactjs-social-login";
-
 
 function Login() {
   return (
-    <div> <LoginSocialGoogle
-    client_id={"138949418546-q56sjf702pnfvp5e1sun4af79jq5f5mr.apps.googleusercontent.com"}
-    scope="openid profile email"
-    discoveryDocs="claims_supported"
-    access_type="offline"
-    onResolve={({ provider, data }) => {
-      console.log(provider, data);
-    }}
-    onReject={(err) => {
-      console.log(err);
-    }}
-  >
-    <GoogleLoginButton />
-  </LoginSocialGoogle></div>
+    <div>Login</div>
   )
 }
 
