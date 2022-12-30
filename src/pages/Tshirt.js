e6e2c2fd6222381d056969c2d5374dc295dbf1ac
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-12-31 03:40:28 +0530

    initial commitlkml

--- src/pages/Tshirt.js
+++ src/pages/Tshirt.js
@@ -2,13 +2,26 @@ import React from 'react'
 import Dnav from '../components/Dnav'
 import Searchbar from '../components/Searchbar'
 import Sort from '../components/Sort'
+import red from '../../src/red.jpg'
 
 function Tshirt() {
   return (
     <>
     <Dnav page="T Shirts"/>
-    <Sort />
-    
+<div className='row'>
+<div className='col-6'>
+    <img src={red} alt='a' className='' height='38%' />
+
+</div>
+<div className='col-6'>
+<img src={red} alt='a' height='38%' />
+
+</div>
+</div>
+
+
+
+    <Sort />   
     </>
   )
 }
