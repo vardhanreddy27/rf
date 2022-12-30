f1778d419cd6848d454226f4985f2427689ef065
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-12-31 04:14:20 +0530

    initial commitlkmlll

--- src/pages/Tshirt.js
+++ src/pages/Tshirt.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Dnav from '../components/Dnav'
-import Searchbar from '../components/Searchbar'
 import Sort from '../components/Sort'
 import red from '../../src/red.jpg'
+import { AiOutlineHeart } from 'react-icons/ai';
 
 function Tshirt() {
   return (
@@ -10,15 +10,34 @@ function Tshirt() {
     <Dnav page="T Shirts"/>
 <div className='row'>
 <div className='col-6'>
-    <img src={red} alt='a' className='' height='38%' />
+    <img src={red} alt='a' className='' width='99%' />
 
 </div>
 <div className='col-6'>
-<img src={red} alt='a' height='38%' />
+<img src={red} alt='a' width='99%' />
 
 </div>
 </div>
-
+<div className='row ms-1 mt-2'>
+    
+<div className='col-6'>
+<div className='row'>
+<div className='col-10'>
+   <h6 className='mypc1 mn'> roadster</h6>
+   <p>Round Neck T-shirt</p>
+   <s className='text-secondary'>₹499</s><p className='mypc1 d-inline'>  ₹224</p></div>
+     <div className='col-2'>
+     <AiOutlineHeart className='mt-1 mb-1 ss'/></div></div></div>
+<div className='col-6'>
+<div className='row'>
+<div className='col-10'>
+   <h6 className='mypc1 mn'> roadster</h6>
+   <p>Round Neck T-shirt</p>
+   <s className='text-secondary'>₹699</s><p className='mypc1 d-inline'>  ₹345</p></div>
+     <div className='col-2'>
+     <AiOutlineHeart className='mt-1 mb-1 ss'/></div></div>
+</div>
+</div>
 
 
     <Sort />   
