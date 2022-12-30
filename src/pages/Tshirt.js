f67d61e603a6cb6f7ac3a473a1923dae7b2a8ee6
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-12-31 04:25:51 +0530

    initial commitlkmllls

--- src/pages/Tshirt.js
+++ src/pages/Tshirt.js
@@ -8,7 +8,7 @@ function Tshirt() {
   return (
     <>
     <Dnav page="T Shirts"/>
-<div className='row'>
+    <div className='row'>
 <div className='col-6'>
     <img src={red} alt='a' className='' width='99%' />
 
@@ -25,19 +25,80 @@ function Tshirt() {
 <div className='col-10'>
    <h6 className='mypc1 mn'> roadster</h6>
    <p>Round Neck T-shirt</p>
-   <s className='text-secondary'>₹499</s><p className='mypc1 d-inline'>  ₹224</p></div>
+   <s className='text-secondary'>₹499</s><p className='mypc1 d-inline'>  ₹224</p><p className='d-inline text-success'> 45% OFF</p></div>
      <div className='col-2'>
      <AiOutlineHeart className='mt-1 mb-1 ss'/></div></div></div>
 <div className='col-6'>
 <div className='row'>
 <div className='col-10'>
-   <h6 className='mypc1 mn'> roadster</h6>
+   <h6 className='mypc1 mn'> puma</h6>
    <p>Round Neck T-shirt</p>
-   <s className='text-secondary'>₹699</s><p className='mypc1 d-inline'>  ₹345</p></div>
+   <s className='text-secondary'>₹699</s><p className='mypc1 d-inline'>  ₹345</p><p className='d-inline text-success'> 35% OFF</p></div>
      <div className='col-2'>
      <AiOutlineHeart className='mt-1 mb-1 ss'/></div></div>
 </div>
 </div>
+<div className='row mt-3'>
+<div className='col-6'>
+    <img src={red} alt='a' className='' width='99%' />
+
+</div>
+<div className='col-6'>
+<img src={red} alt='a' width='99%' />
+
+</div>
+</div>
+<div className='row ms-1 mt-2'>
+    
+<div className='col-6'>
+<div className='row'>
+<div className='col-10'>
+   <h6 className='mypc1 mn'> nike</h6>
+   <p>Round Neck T-shirt</p>
+   <s className='text-secondary'>₹699</s><p className='mypc1 d-inline'>  ₹424</p><p className='d-inline text-success'> 27% OFF</p></div>
+     <div className='col-2'>
+     <AiOutlineHeart className='mt-1 mb-1 ss'/></div></div></div>
+<div className='col-6'>
+<div className='row'>
+<div className='col-10'>
+   <h6 className='mypc1 mn'> adidas</h6>
+   <p>Round Neck T-shirt</p>
+   <s className='text-secondary'>₹559</s><p className='mypc1 d-inline'>  ₹345</p><p className='d-inline text-success'> 17% OFF</p></div>
+     <div className='col-2'>
+     <AiOutlineHeart className='mt-1 mb-1 ss'/></div></div>
+</div>
+</div>
+<div className='row mt-3'>
+<div className='col-6'>
+    <img src={red} alt='a' className='' width='99%' />
+
+</div>
+<div className='col-6'>
+<img src={red} alt='a' width='99%' />
+
+</div>
+</div>
+<div className='row ms-1 mt-2'>
+    
+<div className='col-6'>
+<div className='row'>
+<div className='col-10'>
+   <h6 className='mypc1 mn'> highlander</h6>
+   <p>Round Neck T-shirt</p>
+   <s className='text-secondary'>₹499</s><p className='mypc1 d-inline'>  ₹224</p><p className='d-inline text-success'> 45% OFF</p></div>
+     <div className='col-2'>
+     <AiOutlineHeart className='mt-1 mb-1 ss'/></div></div></div>
+<div className='col-6'>
+<div className='row'>
+<div className='col-10'>
+   <h6 className='mypc1 mn'> wrong</h6>
+   <p>Round Neck T-shirt</p>
+   <s className='text-secondary'>₹399</s><p className='mypc1 d-inline'>  ₹245</p><p className='d-inline text-success'> 19% OFF</p></div>
+     <div className='col-2'>
+     <AiOutlineHeart className='mt-1 mb-1 ss'/></div></div>
+</div>
+</div>
+<div className='mtp'>s</div>
 
 
     <Sort />   
