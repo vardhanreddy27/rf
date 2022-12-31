1328f372d41ffac6516f4b4063cf05478c729cc0
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-12-31 12:53:26 +0530

    initial commitlkmllaayy

--- src/pages/Tshirt.js
+++ src/pages/Tshirt.js
@@ -1,6 +1,8 @@
 import {React,useState} from 'react'
 import Dnav from '../components/Dnav'
 import Sort from '../components/Sort'
+import { AiFillHeart } from 'react-icons/ai';
+
 import red from '../../src/red.jpg'
 import orange from '../../src/orange.jpg'
 import black from '../../src/black.jpg'
@@ -8,19 +10,17 @@ import yellow from '../../src/yellow.jpg'
 import pink from '../../src/pink.jpg'
 import blue from '../../src/blue.jpg'
 import { AiOutlineHeart } from 'react-icons/ai';
-import { checkPropTypes } from 'prop-types'
 
 function Tshirt() {
-  function  checks(){
-    setColor(color + 1);
-    if(color % 2 == 0){
-    console.log('red')
-}
-else{
-    console.log('white' )
-}
-}
-    var [color,setColor]= useState(0);
+    const [isActive, setIsActive] = useState(false);
+    const [is1Active, setIs1Active] = useState(false);
+    const [is2Active, setIs2Active] = useState(false);
+    const [is3Active, setIs3Active] = useState(false);
+    const [is4Active, setIs4Active] = useState(false);
+    const [is5Active, setIs5Active] = useState(false);
+
+    var activeIcon=<AiFillHeart  color="red"/>;
+    var inactiveIcon=<AiOutlineHeart />;
   return (
     <>
     <Dnav page="T Shirts"/>
@@ -43,8 +43,8 @@ else{
    <p>Round Neck T-shirt</p>
    <s className='text-secondary'>₹499</s><p className='mypc1 d-inline'>  ₹224</p><p className='d-inline text-success'> 45% OFF</p></div>
      <div className='col-2'>
-     <button className='bco' onClick={checks}>
- <AiOutlineHeart className='mt-1 mb-1 stc'/></button></div></div></div>
+     <button className='bco' onClick={() => setIsActive(!isActive)}>{isActive ? activeIcon : inactiveIcon}
+</button></div></div></div>
 <div className='col-6'>
 <div className='row'>
 <div className='col-10'>
@@ -52,8 +52,8 @@ else{
    <p>Round Neck T-shirt</p>
    <s className='text-secondary'>₹699</s><p className='mypc1 d-inline'>  ₹345</p><p className='d-inline text-success'> 35% OFF</p></div>
      <div className='col-2'>
-     <button className='bco' onClick={checks}>
- <AiOutlineHeart className='mt-1 mb-1 stc'/></button></div></div>
+     <button className='bco'  onClick={() => setIs1Active(!is1Active)}>{is1Active ? activeIcon : inactiveIcon}
+ </button></div></div>
 </div>
 </div>
 <div className='row mt-3'>
@@ -74,8 +74,8 @@ else{
    <p>Round Neck T-shirt</p>
    <s className='text-secondary'>₹699</s><p className='mypc1 d-inline'>  ₹424</p><p className='d-inline text-success'> 27% OFF</p></div>
      <div className='col-2'>
-     <button className='bco' onClick={checks}>
- <AiOutlineHeart className='mt-1 mb-1 stc'/></button></div></div></div>
+     <button className='bco' onClick={() => setIs2Active(!is2Active)}>{is2Active ? activeIcon : inactiveIcon}
+ </button></div></div></div>
 <div className='col-6'>
 <div className='row'>
 <div className='col-10'>
@@ -83,8 +83,8 @@ else{
    <p>Round Neck T-shirt</p>
    <s className='text-secondary'>₹559</s><p className='mypc1 d-inline'>  ₹345</p><p className='d-inline text-success'> 17% OFF</p></div>
      <div className='col-2'>
-     <button className='bco' onClick={checks}>
- <AiOutlineHeart className='mt-1 mb-1 stc'/></button></div></div>
+     <button className='bco' onClick={() => setIs3Active(!is3Active)}>{is3Active ? activeIcon : inactiveIcon}
+</button></div></div>
 </div>
 </div>
 <div className='row mt-3'>
@@ -106,8 +106,8 @@ else{
    <p>Round Neck T-shirt</p>
    <s className='text-secondary'>₹499</s><p className='mypc1 d-inline'>  ₹224</p><p className='d-inline text-success'> 45% OFF</p></div>
      <div className='col-2'>
-     <button className='bco' onClick={checks}>
- <AiOutlineHeart className='mt-1 mb-1 stc'/></button></div></div></div>
+     <button className='bco'onClick={() => setIs4Active(!is4Active)}>{is4Active ? activeIcon : inactiveIcon}
+</button></div></div></div>
 <div className='col-6'>
 <div className='row'>
 <div className='col-10'>
@@ -115,8 +115,8 @@ else{
    <p>Round Neck T-shirt</p>
    <s className='text-secondary'>₹399</s><p className='mypc1 d-inline'>  ₹245</p><p className='d-inline text-success'> 19% OFF</p></div>
      <div className='col-2'>
-     <button className='bco' onClick={checks}>
- <AiOutlineHeart className='mt-1 mb-1 stc'/></button>
+     <button className='bco' onClick={() => setIs5Active(!is5Active)}>{is5Active ? activeIcon : inactiveIcon}
+ </button>
  </div></div>
 </div>
 </div>
